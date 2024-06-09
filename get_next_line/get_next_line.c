7ec740950c84374552d64eed2ade13cea7454101
Pablodoblem <pablodoblem@gmial.com>
2024-06-09 12:54:17 +0200

    Nueva versión sin segfaults 2 tests gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pamarti2 <pamarti2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 00:46:54 by pamarti2          #+#    #+#             */
-/*   Updated: 2024/06/09 01:58:25 by pamarti2         ###   ########.fr       */
+/*   Updated: 2024/06/09 12:53:00 by pamarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,10 @@ int	check_nl_and_null(char *buffer)
 	int	counter;
 
 	counter = 0;
-    while (buffer[counter])
-    {
-        if (buffer[counter] == '\n' || buffer[counter] == '\0')
-            return counter;
+	while (buffer[counter])
+	{
+		if (buffer[counter] == '\n' || buffer[counter] == '\0')
+			return counter;
 		counter++;
 	}
 	return (counter);
@@ -52,18 +52,30 @@ node	*create_new_node(char *buffer, int n_chars_buf)
 	node	*new_node;
 	
 	new_node = (node *)malloc(sizeof(node));
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 	new_node->next = NULL;
 	new_node->string_piece = malloc((n_chars_buf + 1) * sizeof(char));
-	// if (new_node->string_piece == NULL)
-	// {
-	// 	free(new_node);
-	// 	return (NULL);
-	// }
+	if (!new_node->string_piece)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	strncpy(new_node->string_piece, buffer, n_chars_buf);
 	new_node->string_piece[n_chars_buf] = '\0';
 	return (new_node);
+}
+
+void free_all_nodes(node *head)
+{
+    node *tmp;
+    while (head)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp->string_piece);
+        free(tmp);
+    }
 }
 
 char	*get_next_line(int fd)
@@ -89,15 +101,15 @@ char	*get_next_line(int fd)
 	{
 		memset(buffer, 0, 11);
 		char_read = read(fd, buffer, 10);
-		printf("Lineas de buffer: ");
-		printf("%s\n", buffer);
-		if (char_read == 0)
-			break ;
-		printf("Chars_read: %d\n", char_read);
+		//printf("Lineas de buffer: ");
+		//printf("%s\n", buffer);
+		// if (char_read == 0)
+		// 	break ;
+		//printf("Chars_read: %d\n", char_read);
 		n_chars_buf = check_nl_and_null(buffer);
-		printf("Valor n_chars_buf: %d\n", n_chars_buf);
+		//printf("Valor n_chars_buf: %d\n", n_chars_buf);
 		new_node = create_new_node(buffer, n_chars_buf);
-		printf("String de new_node->string_piece: %s\n", new_node->string_piece);
+		//printf("String de new_node->string_piece: %s\n", new_node->string_piece);
 		if (head == NULL)
 			head = new_node;
 		else
@@ -113,10 +125,10 @@ char	*get_next_line(int fd)
 				{
 					aux_ptr++;
 					new_node = create_new_node(aux_ptr, (10 - n_chars_buf)); //será asignado como head al final del if
-					printf("String residual que encabezará nueva línea: %s\n", new_node->string_piece);  // print de string residual para head
+					//printf("String residual que encabezará nueva línea: %s\n", new_node->string_piece);  // print de string residual para head
 				}
 			}
-			printf("\n-------------------\n");
+			//printf("\n-------------------\n");
 			printf("LÍNEA %d --> ", x);
 			current = head;
 			while (current != NULL) //printear las cadenas desde head
@@ -124,28 +136,33 @@ char	*get_next_line(int fd)
 				printf("%s", current->string_piece);
 				current = current->next;
 			}
-			printf("\n-------------------\n");
+			free_all_nodes(head);
+			printf("\n");
+			//printf("\n-------------------\n");
 			current = head;
-			while (current != NULL) //liberar memoria de nodos de linea printeada
-			{
-				node *next = current->next;
-				free(current->string_piece);
-				free(current);  //Momento en el que se sibera el espacio de memoria (leak)
-				current = next;
-			}
+			// while (current != NULL) //liberar memoria de nodos de linea printeada
+			// {
+			// 	node *next = current->next;
+			// 	free(current->string_piece);
+			// 	free(current);
+			// 	current = next;
+			// }
 			//printf("ESTE ES EL VALOR DE 'NEW_NODE': %s\n", new_node->string_piece);
 			printf("\n");
 			head = new_node; //head apunta al "trozo" que resta del buffer donde hay salto de linea
 			current = new_node;
 			//i--;
 			//break ;
-			if (strchr(new_node->string_piece, 10) != NULL) //Aquí es donde tengo el leak. Trato de leer 8bytes en l región *00210 cuando esta ya ha sido liberada previamente
+			if (char_read < 10) //Solución para que al final de la lectura no haya segfault.
+				break ;
+			if (strchr(new_node->string_piece, 10) != NULL)
 			{
 				x++;
-				printf("ESTA ES LA EXCEPCIÓN PARA CUANDO HAY SALTO DE LINEA\n AL FINAL DE LA STRING RESIDUAL:");
-				printf("\n-------------------\n");
+				//printf("ESTA ES LA EXCEPCIÓN PARA CUANDO HAY SALTO DE LINEA\n AL FINAL DE LA STRING RESIDUAL:");
+				//printf("\n-------------------\n");
 				printf("LÍNEA %d --> %s", x, new_node->string_piece);
-				printf("-------------------\n");
+				printf("\n");
+				//printf("\n-------------------\n");
 				free(new_node->string_piece);
 				free(new_node);
 				head = NULL;
@@ -156,7 +173,7 @@ char	*get_next_line(int fd)
 			current = new_node;
 	}
 	//printf("%s\n", strcpy(new_node->string_piece, buffer));
-	//close(fd);
+	close(fd);
 	return (0);
 }
 
